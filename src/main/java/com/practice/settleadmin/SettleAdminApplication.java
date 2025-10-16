83474f583260a41357f386bb28aa5a68fd57e13d
juneunbae <dmsqo1403@naver.com>
2025-10-16 23:57:19 +0900

    first commit

---
+++ src/main/java/com/practice/settleadmin/SettleAdminApplication.java
@@ -0,0 +1,13 @@
+package com.practice.settleadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettleAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettleAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/settleadmin/SettleAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.settleadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettleAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
